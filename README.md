6a3701c4dcf3807e5daee19c8fc8a140e3042839
Pascal Chorus <p.chorus@me.com>
2016-11-01 11:02:04 +0100

    initial commit containing a first experimental version of TeamCity Monitor

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # teamcity-monitor-dist
 TeamCity Monitor Distribution Package
+
+# Running the TeamCity Monitor
+To run the TeamCity Monitor you must have an actual installation of Node.js.
+You can download it here: https://nodejs.org.
+
+Then you have to install two additional Node.js Packages using the following commands.
+npm install express
+npm install cors
+
+Afterwards the server can be started by running the command
+node server.js
+
+# Configure your Server
+The configuration of the TeamCity Monitor is stored in the config.json file.
+TODO
---
+++ server.js
@@ -0,0 +1,41 @@
+var express = require('express'),
+  cors = require('cors'),
+  app = express(),
+  http,
+  fs = require('fs'),
+  config = JSON.parse(fs.readFileSync('config.json').toString());
+
+http = config.tls ? require('https') : require('http');
+
+app.use('/', express.static('static'));
+
+app.get('/buildTypes', cors(), function (req, res, next) {
+  var str = '';
+  http.get({
+    hostname: config.hostname,
+    path: '/guestAuth/app/rest/buildTypes?fields=buildType(id,name,builds($locator(running:false,canceled:false,count:1),build(number,status)))',
+    headers: {
+      Accept: 'application/json'
+    }
+  }, response => {
+    response.on('data', function (d) {
+      str += d;
+    });
+
+    response.on('end', function (d) {
+      let buildTypes = JSON.parse(str),
+        ret = buildTypes.buildType.filter(element => config.buildTypes.indexOf(element.id) != -1);
+
+      res.send(ret);
+    });
+  });
+});
+
+app.get('/buildTypesConfig', cors(), function (req, res, next) {
+  let data = fs.readFileSync('./config.json');
+  res.send(data);
+});
+
+app.listen(8080, function(){
+  console.log('CORS-enabled web server listening on port 8080');
+});
